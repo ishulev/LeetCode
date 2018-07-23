393f3a4ea21b88d7f615fdd8494455357db3add1
Ivan Shulev <ishulev@melontech.com>
2018-07-23 18:16:28 +0300

    wip

--- 05.longest-palindromic-substring.js
+++ 05.longest-palindromic-substring.js
@@ -2,57 +2,99 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {
-    if (s.length === 1) {
-        return s;
+var longestPalindrome = function(s) {
+  if (s.length === 1) {
+    return s;
+  }
+  let palindrome = '';
+  let arrayWithRepeatingChars = [];
+
+  for (var i = 0; i < s.length; i++) {
+    const item = s[i];
+    if (s.indexOf(item, i + 1) !== -1) {
+      arrayWithRepeatingChars += item;
     }
-    const arrayFromString = s.split('');
-    let palindromeArray = [];
-    let arrayWithRepeatingChars = arrayFromString.filter(function (item, index) {
-        return arrayFromString.indexOf(item, index + 1) !== -1;
+  }
+
+  //   if (s.length === arrayWithRepeatingChars.length + 1) {
+  //     return s;
+  //   }
+  // arrayWithRepeatingChars = arrayFromString.filter(function (item, index) {
+  //     return arrayFromString.indexOf(item, index + 1) !== -1;
+  // });
+  //   const arrayFromString = s.split('');
+
+  arrayWithRepeatingChars = arrayWithRepeatingChars
+    .split('')
+    .filter(function(item, index) {
+      const nextItemIndex = arrayWithRepeatingChars.indexOf(item, index + 1);
+      return nextItemIndex === -1;
     });
 
-    arrayWithRepeatingChars = arrayWithRepeatingChars.filter(function (item, index) {
-        const nextItemIndex = arrayWithRepeatingChars.indexOf(item, index + 1);
-        return nextItemIndex === -1;
-    });
+  let outerWhileCounter = 0;
+  let innerWhileCounter = 0;
 
-    arrayWithRepeatingChars.forEach(function (item) {
-        let firstIndex = arrayFromString.indexOf(item);
-        let finalIndex = arrayFromString.lastIndexOf(item);
-        console.log('beforeLoop');
-        while (firstIndex > -1 && firstIndex < finalIndex) {
-            console.log('outerLoopStart');
-            while (finalIndex > -1 && finalIndex !== firstIndex) {
-                console.log('innerLoopStart');
-                const possiblePolindrome = arrayFromString.slice(firstIndex, finalIndex + 1);
-                let oddLength = true;
-                if (possiblePolindrome.length % 2) {
-                    oddLength = false;
-                }
-                const numOfItemsOnOneSide = Math.floor(possiblePolindrome.length / 2);
-                const itemsOnLeft = possiblePolindrome.slice(0, numOfItemsOnOneSide);
-                const itemsOnRight = possiblePolindrome.slice(oddLength ? numOfItemsOnOneSide : numOfItemsOnOneSide + 1, finalIndex + 1);
-                const leftString = itemsOnLeft.join('');
-                const rightString = itemsOnRight.reverse().join('');
-                if (leftString === rightString) {
-                    if (possiblePolindrome.length > palindromeArray.length) {
-                        palindromeArray = possiblePolindrome;
-                    }
-                }
-                finalIndex = arrayFromString.lastIndexOf(item, finalIndex - 1);
-            }
-            firstIndex = arrayFromString.indexOf(item, firstIndex + 1);
-            finalIndex = arrayFromString.lastIndexOf(item);
+  //   arrayWithRepeatingChars = Array.from(new Set(Array.from(arrayWithRepeatingChars)));
+
+  arrayWithRepeatingChars.forEach(function(item) {
+    //   console.log(item);
+    let firstIndex = s.indexOf(item);
+    let finalIndex = s.lastIndexOf(item);
+    while (firstIndex > -1 && firstIndex < finalIndex) {
+      while (finalIndex > -1 && finalIndex !== firstIndex) {
+        const possiblePolindrome = s.substring(firstIndex, finalIndex + 1);
+        let oddLength = true;
+        if (possiblePolindrome.length % 2) {
+          oddLength = false;
         }
-        console.log('afterLoop');
-    });
-    return palindromeArray.join('') || arrayFromString[0];
+        const numOfItemsOnOneSide = Math.floor(possiblePolindrome.length / 2);
+        const itemsOnLeft = possiblePolindrome.substring(
+          0,
+          numOfItemsOnOneSide
+        );
+        const itemsOnRight = possiblePolindrome.substring(
+          oddLength ? numOfItemsOnOneSide : numOfItemsOnOneSide + 1,
+          finalIndex + 1
+        );
+        if (itemsOnLeft === reverseString(itemsOnRight)) {
+          if (possiblePolindrome.length > palindrome.length) {
+            palindrome = possiblePolindrome;
+          }
+        }
+        finalIndex = s.lastIndexOf(item, finalIndex - 1);
+        innerWhileCounter++;
+      }
+      firstIndex = s.indexOf(item, firstIndex + 1);
+      finalIndex = s.lastIndexOf(item);
+      outerWhileCounter++;
+    }
+  });
+  console.log('OUTER =>> ', outerWhileCounter);
+  console.log('INNER =>> ', innerWhileCounter);
+  return palindrome || s[0];
 };
+
+function reverseString(str) {
+  let returnString = '';
+  for (var i = str.length - 1; i >= 0; i--) {
+    returnString += str[i];
+  }
+  return returnString;
+}
 
 // console.log(longestPalindrome('dbagabbaagaab'));
 // console.log(longestPalindrome('cbbd'));
 // console.log(longestPalindrome('bb'));
 // console.log(longestPalindrome('babad'));
-longestPalindrome('abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa');
-
+console.log(longestPalindrome('aaaabaaa'));
+// console.log(longestPalindrome('abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa'));
+// console.log(
+//   longestPalindrome(
+//     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+//   )
+// );
+// console.log(
+//   longestPalindrome(
+//     '321012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210123210012321001232100123210123'
+//   )
+// );
