aba057496a153619457439e560c5c5fa682091bf
Ivan Shulev <ishulev@melontech.com>
2018-07-27 19:52:20 +0300

    solution for 6

--- tasks/06.zigzag-conversion.js
+++ tasks/06.zigzag-conversion.js
@@ -4,12 +4,17 @@
  * @return {string}
  */
 var convert = function (s, numRows) {
+    // This is for edge cases, where string length is less than rows, rows are less than 2 or string has length of 1
+    if(s.length < numRows || numRows < 2 || s.length < 2) {
+        return s;
+    }
     let returnS = '';
+    // Loop over rows
     for (let j = 0; j < numRows; j++) {
         for (let i = j; i < s.length; i += 2 * (numRows - 1)) {
             returnS += s[i];
             if (j !== 0 && j < numRows - 1) {
-                const intermediateIndex = i + 2 * j;
+                const intermediateIndex = i + 2 * (numRows - j - 1);
                 if (s[intermediateIndex]) {
                     returnS += s[intermediateIndex];
                 }
--- test/06.test.js
+++ test/06.test.js
@@ -1,13 +1,29 @@
 const convert = require('../tasks/06.zigzag-conversion');
 
+test('convert(A, 1) equals A', () => {
+    expect(convert('A', 1)).toBe('A');
+});
+
 test('convert(PAYPALZ, 3) equals PAAPLYZ', () => {
     expect(convert('PAYPALZ', 3)).toBe('PAAPLYZ');
+});
+
+test('convert(PAYPALZ, 2) equals PYAZAPL', () => {
+    expect(convert('PAYPALZ', 2)).toBe('PYAZAPL');
 });
 
 test('convert(PAYPALISHIRING, 3) equals PAHNAPLSIIGYIR', () => {
     expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
 });
 
-test('convert(PAYPALISHIRING, 5) equals PRAIIYHNPSGAIL', () => {
-    expect(convert('PAYPALISHIRING', 5)).toBe('PRAIIYHNPSGAIL');
+test('convert(PAYPALISHIRING, 4) equals PINALSIGYAHRPI', () => {
+    expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
+});
+
+test('convert(PAYPALISHIRING, 5) equals PHASIYIRPLIGAN', () => {
+    expect(convert('PAYPALISHIRING', 5)).toBe('PHASIYIRPLIGAN');
+});
+
+test('convert(PAYPALISHIRING, 6) equals PRAIIYHNPSGAIL', () => {
+    expect(convert('PAYPALISHIRING', 6)).toBe('PRAIIYHNPSGAIL');
 });
